acf456a898b96ba957fdd2c634c7b36b39410bf4
anushayadav01 <Anusha_Yadav@mentor.com>
2022-09-30 11:46:50 +0530

    third commit

--- tests/clienta.py
+++ tests/clienta.py
@@ -8,13 +8,15 @@ class ClientA(MQTTRPC):
 
     async def run_hello_on_b(self, name):
         client_b = self.get_proxy_for('client_b')
-        print(client_b)
+        print("client b:", client_b)
         print(type(name))
+        # return await client_b.hello(name.decode('utf-8'))
         return await client_b.hello(name)
 
 
 loop = asyncio.get_event_loop()
 client_a = ClientA(client_uid='client_a')
+client_a.publish('rpc/client_a/client_b/reply',"on")
 asyncio.ensure_future(client_a.process_messages())
 print(
     loop.run_until_complete(
